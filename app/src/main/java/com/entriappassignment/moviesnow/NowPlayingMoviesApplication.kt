955e65e66a3fc47ecf7443ad3ee7b128b5b4361e
Saneen K P <saneenkp2000@gmail.com>
2022-09-11 19:19:08 +0530

    gradle updated for hilt, dagger and kotlin extensions

---
+++ app/src/main/java/com/entriappassignment/moviesnow/NowPlayingMoviesApplication.kt
@@ -0,0 +1,8 @@
+package com.entriappassignment.moviesnow.hilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NowPlayingMoviesApplication : Application() {
+}
