d5af6f7743cf7bb712c7e04d1fc8cad318c13bc6
Saneen K P <saneenkp2000@gmail.com>
2022-09-11 22:14:03 +0530

    retrofit refractored

--- app/src/main/java/com/entriappassignment/moviesnow/retrofit/MoviesApiService.kt
+++ app/src/main/java/com/entriappassignment/moviesnow/retrofit/MoviesApiService.kt
@@ -8,7 +8,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface MoviesApiServiceRx {
+interface MoviesApiService {
     @GET("movie/now_playing")
     suspend fun getNowPlayingMovies(@Query("page") pageNo : Int): NowPlayingMoviesData
 }
--- app/src/main/java/com/entriappassignment/moviesnow/retrofit/MoviesRetrofitClient.kt
+++ app/src/main/java/com/entriappassignment/moviesnow/retrofit/MoviesRetrofitClient.kt
@@ -1,64 +1,40 @@
 package com.entriappassignment.moviesnow.retrofit
 
-import android.util.Log
-import com.entriappassignment.moviesnow.models.MovieData
+import com.entriappassignment.moviesnow.BuildConfig.API_KEY
+import com.entriappassignment.moviesnow.BuildConfig.BASE_URL
 import com.entriappassignment.moviesnow.models.NowPlayingMoviesData
-import com.google.gson.Gson
-import io.reactivex.Observable
-import io.reactivex.Single
-import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.create
 import java.util.concurrent.TimeUnit
 
 
 class MoviesRetrofitClient() {
 
-    private val BASE_URL = "https://api.themoviedb.org/3/"
-    private val API_KEY = "9a976526fce8c29aaa35eb4a1e654d3c"
-
-    private var moviesApiServiceRx : MoviesApiServiceRx
-    private var gsonConverterFactory : GsonConverterFactory
-    private var requestInterceptor : Interceptor
-
-    init {
-        moviesApiServiceRx = getMoviesApiServiceRx()
-        gsonConverterFactory = getGsonConverterFactory()
-        requestInterceptor = getInterceptor()
-    }
-
     private fun getInterceptor() : Interceptor{
-        if (requestInterceptor == null){
-           requestInterceptor = Interceptor{
+       val requestInterceptor = Interceptor{
 
-               val url = it.request()
-                   .url
-                   .newBuilder()
-                   .addQueryParameter("api_key" , API_KEY)
-                   .build()
+           val url = it.request()
+               .url
+               .newBuilder()
+               .addQueryParameter("api_key" , API_KEY)
+               .build()
 
-               val request = it.request()
-                   .newBuilder()
-                   .url(url)
-                   .build()
+           val request = it.request()
+               .newBuilder()
+               .url(url)
+               .build()
 
-               return@Interceptor it.proceed(request)
-           }
-        }
+           return@Interceptor it.proceed(request)
+       }
         return requestInterceptor
     }
 
     private fun getGsonConverterFactory() : GsonConverterFactory{
-        if (gsonConverterFactory == null){
-            gsonConverterFactory = GsonConverterFactory.create();
-        }
-        return gsonConverterFactory
+        return GsonConverterFactory.create()
     }
 
     private fun getOkHttpClient() : OkHttpClient{
@@ -74,21 +50,15 @@ class MoviesRetrofitClient() {
         return okHttpClient
     }
 
-    private fun getMoviesApiServiceRx() : MoviesApiServiceRx{
+    private fun getMoviesApiServiceRx() : MoviesApiService{
+        var retrofit : Retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(getOkHttpClient())
+            .addConverterFactory(getGsonConverterFactory())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
 
-        if (moviesApiServiceRx == null){
-
-            var retrofit : Retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .client(getOkHttpClient())
-                .addConverterFactory(getGsonConverterFactory())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build()
-
-            moviesApiServiceRx = retrofit.create(MoviesApiServiceRx::class.java)
-        }
-
-        return moviesApiServiceRx
+        return  retrofit.create(MoviesApiService::class.java)
     }
 
     suspend fun getNowPlayingMovies(pageNo : Int): NowPlayingMoviesData {
