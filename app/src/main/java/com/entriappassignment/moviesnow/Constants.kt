3d2d80b564833284ba38e136cbf78282d95b4a8c
Saneen K P <saneenkp2000@gmail.com>
2022-09-11 22:57:09 +0530

    added constants

--- app/src/main/java/com/entriappassignment/moviesnow/Constants.kt
+++ app/src/main/java/com/entriappassignment/moviesnow/Constants.kt
@@ -2,10 +2,7 @@ package com.entriappassignment.moviesnow
 
 class Constants {
     companion object{
-       enum class Status {
-           LOADING,
-           SUCCESS,
-           ERROR
-       }
+        const val MAX_PAGE_COUNT = 100 //maximum no of pages to be loaded to pagingData ,after this the pages would be dropped
+        const val PAGE_SIZE = 20 //no of pages loaded at once
     }
 }
--- app/src/main/java/com/entriappassignment/moviesnow/repository/MovieRepository.kt
+++ app/src/main/java/com/entriappassignment/moviesnow/repository/MovieRepository.kt
@@ -3,6 +3,7 @@ package com.entriappassignment.moviesnow.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.liveData
+import com.entriappassignment.moviesnow.Constants
 import com.entriappassignment.moviesnow.paging.MoviePagingSource
 import com.entriappassignment.moviesnow.retrofit.MoviesRetrofitClient
 import javax.inject.Inject
@@ -10,7 +11,7 @@ import javax.inject.Inject
 class MovieRepository @Inject constructor(val moviesRetrofitClient: MoviesRetrofitClient) {
 
     fun getMovies() = Pager(
-        config = PagingConfig(pageSize = 20 , maxSize = 100),
+        config = PagingConfig(pageSize = Constants.PAGE_SIZE, maxSize = Constants.MAX_PAGE_COUNT),
         pagingSourceFactory = { MoviePagingSource(moviesRetrofitClient) }
     ).liveData
 }
