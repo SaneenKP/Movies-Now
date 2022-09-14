7c1c9e1090cbde1b814a003178590204dee826b3
Saneen K P <saneenkp2000@gmail.com>
2022-09-15 03:41:14 +0530

    formatted movie details with comments

--- app/src/main/java/com/entriappassignment/moviesnow/ui/MovieDetails.kt
+++ app/src/main/java/com/entriappassignment/moviesnow/ui/MovieDetails.kt
@@ -42,26 +42,40 @@ class MovieDetails : AppCompatActivity(), View.OnClickListener {
 
         this.movieDetailsData = data
         if (!movieDetailsContainer.isVisible){
+            somethingWentWrongContainer.visibility = View.GONE
             noConnectionContainer.visibility = View.GONE
             movieDetailsContainer.visibility = View.VISIBLE
         }
 
+        //if movie details is null , show something went wrong
         if (this.movieDetailsData != null){
-
-            //change the tagline value by adding double quotations
-            this.movieDetailsData!!.tagline = "\"${this.movieDetailsData!!.tagline}\""
-            movieDetailsBinding.movieDetails = this.movieDetailsData
-
-            //change vote average value to fit the rating view.
-            this.movieDetailsData!!.voteAverage = Utils.calculateRating(this.movieDetailsData!!.voteAverage!!)
-
-            movieDetailsBinding.movieRuntime.text = Utils.convertMovieRuntimeFormatToString(this.movieDetailsData!!.runtime!!)
-
-            this.movieDetailsData!!.releaseDate = Utils.convertFormatReleaseDate(this.movieDetailsData!!.releaseDate)
-
-            this.movieDetailsData!!.finalGenreString = Utils.getMovieGenreString(this.movieDetailsData!!.genres)
+            formatAndChangeMovieData()
+        }else{
+            movieDetailsContainer.visibility = View.GONE
+            somethingWentWrongContainer.visibility = View.VISIBLE
         }
+
     }
+
+    private fun formatAndChangeMovieData(){
+
+        //change the tagline value by adding double quotations
+        this.movieDetailsData!!.tagline = "\"${this.movieDetailsData!!.tagline}\""
+        movieDetailsBinding.movieDetails = this.movieDetailsData
+
+        //change vote average value to fit the rating view.
+        this.movieDetailsData!!.voteAverage = Utils.calculateRating(this.movieDetailsData!!.voteAverage!!)
+
+        //binding movies runtime after formatting it.
+        movieDetailsBinding.movieRuntime.text = Utils.convertMovieRuntimeFormatToString(this.movieDetailsData!!.runtime!!)
+
+        //convert release date format and bind
+        this.movieDetailsData!!.releaseDate = Utils.convertFormatReleaseDate(this.movieDetailsData!!.releaseDate)
+
+        //combine all the genres into a single string and bind the string.
+        this.movieDetailsData!!.finalGenreString = Utils.getMovieGenreString(this.movieDetailsData!!.genres)
+    }
+
 
     private fun handleIntent(){
         movieId = intent.getIntExtra(Constants.MOVIE_ID , 0)
@@ -69,14 +83,16 @@ class MovieDetails : AppCompatActivity(), View.OnClickListener {
 
     private fun init(){
 
+        //change the status bar color.
         val window = window
-        window.statusBarColor =  applicationContext.resources.getColor(R.color.app_background_color)
+        window.statusBarColor =  applicationContext.resources.getColor(R.color.black)
         movieDetailsViewModel = ViewModelProvider(this)[MovieDetailsViewModel::class.java]
 
         movieDetailsButton.setOnClickListener(this)
     }
 
     private fun setupObservers(){
+        //Observe the movieDetails Retrieved.
         movieDetailsViewModel.getMovieDetails(this.movieId)
         movieDetailsViewModel.movieDetailsResponse.observe(this , Observer {
 
@@ -86,21 +102,25 @@ class MovieDetails : AppCompatActivity(), View.OnClickListener {
 
                    Status.LOADING -> {
                         loadingStatus.visibility = View.VISIBLE
-                        Utils.debug("loading movie details")
                    }
 
                    Status.SUCCESS -> {
                        loadingStatus.visibility = View.GONE
                        movieDetailsContainer.visibility = View.VISIBLE
+
+                       //bind the data received.
                        bindData(response.data)
                    }
 
                    Status.ERROR -> {
+
                        loadingStatus.visibility = View.GONE
                        movieDetailsContainer.visibility = View.GONE
+
+                       //if there is no internet show the internet connectivity error
+                       //else since something else might have caused the error, show the somethingWentWrong error.
                        if (InternetConnectivityState.getInternetConnectivityState()) somethingWentWrongContainer.visibility = View.VISIBLE
                        else noConnectionContainer.visibility = View.VISIBLE
-                       Utils.debug("movie details error ${response.message}")
                    }
                }
            }
@@ -110,6 +130,7 @@ class MovieDetails : AppCompatActivity(), View.OnClickListener {
     override fun onClick(p0: View?) {
         when(p0?.id){
            R.id.movieDetailsButton -> {
+               //open the Url
                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(this.movieDetailsData?.homepage))
                startActivity(browserIntent)
            }
