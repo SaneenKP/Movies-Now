0816cd45f36d377867af973f3195882743cd98a9
Saneen K P <saneenkp2000@gmail.com>
2022-09-15 02:00:29 +0530

    movie database names added with constants

--- app/src/main/java/com/entriappassignment/moviesnow/db/dao/MovieDao.kt
+++ app/src/main/java/com/entriappassignment/moviesnow/db/dao/MovieDao.kt
@@ -6,16 +6,17 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.entriappassignment.moviesnow.models.MovieData
+import com.entriappassignment.moviesnow.utils.Constants
 
 @Dao
 interface MovieDao {
 
-    @Query("SELECT * FROM movie")
+    @Query("SELECT * FROM ${Constants.MOVIE_TABLE_NAME}")
     fun getMovies() : PagingSource<Int, MovieData>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addMovies(movies : List<MovieData>)
 
-    @Query("DELETE FROM movie")
+    @Query("DELETE FROM ${Constants.MOVIE_TABLE_NAME}")
     suspend fun deleteMovie()
 }
